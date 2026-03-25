e7051a58298e27902dd8107b2b7df2f1264a27c2
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-03-25 22:13:22 +0500

    SplayTree.h

--- SplayTree.h
+++ SplayTree.h
@@ -50,3 +50,67 @@ class SplayTree
         makeEmpty( );
         delete nullNode;
     }
+    /**
+     * Deep copy.
+     */
+    SplayTree & operator=( const SplayTree & rhs )
+    {
+        SplayTree copy = rhs;
+        std::swap( *this, copy );
+        return *this;
+    }
+        
+    /**
+     * Move.
+     */
+    SplayTree & operator=( SplayTree && rhs )
+    {
+        std::swap( root, rhs.root );
+        std::swap( nullNode, rhs.nullNode );
+        
+        return *this;
+    }
+    
+    /**
+     * Find the smallest item in the tree.
+     * Not the most efficient implementation (uses two passes), but has correct
+     *     amortized behavior.
+     * A good alternative is to first call find with parameter
+     *     smaller than any item in the tree, then call findMin.
+     * Return the smallest item or throw UnderflowException if empty.
+     */
+    const Comparable & findMin( )
+    {
+        if( isEmpty( ) )
+            throw UnderflowException{ };
+
+        BinaryNode *ptr = root;
+
+        while( ptr->left != nullNode )
+            ptr = ptr->left;
+
+        splay( ptr->element, root );
+        return ptr->element;
+    }
+
+    /**
+     * Find the largest item in the tree.
+     * Not the most efficient implementation (uses two passes), but has correct
+     *     amortized behavior.
+     * A good alternative is to first call find with parameter
+     *     larger than any item in the tree, then call findMax.
+     * Return the largest item or throw UnderflowException if empty.
+     */
+    const Comparable & findMax( )
+    {
+        if( isEmpty( ) )
+            throw UnderflowException{ };
+
+        BinaryNode *ptr = root;
+
+        while( ptr->right != nullNode )
+            ptr = ptr->right;
+
+        splay( ptr->element, root );
+        return ptr->element;
+    }
