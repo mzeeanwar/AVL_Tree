2d676f46502ac9f99dba05e05eb85e74cb156579
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-03-25 22:05:16 +0500

    Update Sort.h

--- Sort.h
+++ Sort.h
@@ -149,3 +149,135 @@ void mergeSort( vector<Comparable> & a )
 
     mergeSort( a, tmpArray, 0, a.size( ) - 1 );
 }
+
+
+/**
+ * Internal method that merges two sorted halves of a subarray.
+ * a is an array of Comparable items.
+ * tmpArray is an array to place the merged result.
+ * leftPos is the left-most index of the subarray.
+ * rightPos is the index of the start of the second half.
+ * rightEnd is the right-most index of the subarray.
+ */
+template <typename Comparable>
+void merge( vector<Comparable> & a, vector<Comparable> & tmpArray,
+            int leftPos, int rightPos, int rightEnd )
+{
+    int leftEnd = rightPos - 1;
+    int tmpPos = leftPos;
+    int numElements = rightEnd - leftPos + 1;
+
+    // Main loop
+    while( leftPos <= leftEnd && rightPos <= rightEnd )
+        if( a[ leftPos ] <= a[ rightPos ] )
+            tmpArray[ tmpPos++ ] = std::move( a[ leftPos++ ] );
+        else
+            tmpArray[ tmpPos++ ] = std::move( a[ rightPos++ ] );
+
+    while( leftPos <= leftEnd )    // Copy rest of first half
+        tmpArray[ tmpPos++ ] = std::move( a[ leftPos++ ] );
+
+    while( rightPos <= rightEnd )  // Copy rest of right half
+        tmpArray[ tmpPos++ ] = std::move( a[ rightPos++ ] );
+
+    // Copy tmpArray back
+    for( int i = 0; i < numElements; ++i, --rightEnd )
+        a[ rightEnd ] = std::move( tmpArray[ rightEnd ] );
+}
+
+
+/**
+ * Return median of left, center, and right.
+ * Order these and hide the pivot.
+ */
+template <typename Comparable>
+const Comparable & median3( vector<Comparable> & a, int left, int right )
+{
+    int center = ( left + right ) / 2;
+    
+    if( a[ center ] < a[ left ] )
+        std::swap( a[ left ], a[ center ] );
+    if( a[ right ] < a[ left ] )
+        std::swap( a[ left ], a[ right ] );
+    if( a[ right ] < a[ center ] )
+        std::swap( a[ center ], a[ right ] );
+
+        // Place pivot at position right - 1
+    std::swap( a[ center ], a[ right - 1 ] );
+    return a[ right - 1 ];
+}
+
+/**
+ * Internal quicksort method that makes recursive calls.
+ * Uses median-of-three partitioning and a cutoff of 10.
+ * a is an array of Comparable items.
+ * left is the left-most index of the subarray.
+ * right is the right-most index of the subarray.
+ */
+template <typename Comparable>
+void quicksort( vector<Comparable> & a, int left, int right )
+{
+    if( left + 10 <= right )
+    {
+        const Comparable & pivot = median3( a, left, right );
+
+            // Begin partitioning
+        int i = left, j = right - 1;
+        for( ; ; )
+        {
+            while( a[ ++i ] < pivot ) { }
+            while( pivot < a[ --j ] ) { }
+            if( i < j )
+                std::swap( a[ i ], a[ j ] );
+            else
+                break;
+        }
+
+        std::swap( a[ i ], a[ right - 1 ] );  // Restore pivot
+
+        quicksort( a, left, i - 1 );     // Sort small elements
+        quicksort( a, i + 1, right );    // Sort large elements
+    }
+    else  // Do an insertion sort on the subarray
+        insertionSort( a, left, right );
+}
+
+/**
+ * Quicksort algorithm (driver).
+ */
+template <typename Comparable>
+void quicksort( vector<Comparable> & a )
+{
+    quicksort( a, 0, a.size( ) - 1 );
+}
+
+
+/**
+ * Internal selection method that makes recursive calls.
+ * Uses median-of-three partitioning and a cutoff of 10.
+ * Places the kth smallest item in a[k-1].
+ * a is an array of Comparable items.
+ * left is the left-most index of the subarray.
+ * right is the right-most index of the subarray.
+ * k is the desired rank (1 is minimum) in the entire array.
+ */
+template <typename Comparable>
+void quickSelect( vector<Comparable> & a, int left, int right, int k )
+{
+    if( left + 10 <= right )
+    {
+        const Comparable & pivot = median3( a, left, right );
+
+            // Begin partitioning
+        int i = left, j = right - 1;
+        for( ; ; )
+        {
+            while( a[ ++i ] < pivot ) { }
+            while( pivot < a[ --j ] ) { }
+            if( i < j )
+                std::swap( a[ i ], a[ j ] );
+            else
+                break;
+        }
+
+        std::swap( a[ i ], a[ right - 1 ] );  // Restore pivot
