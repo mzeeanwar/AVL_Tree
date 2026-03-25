380a2312d2146a8d7ce23cd10fcacffc1f0430f5
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-03-25 22:03:56 +0500

    Create Sort.h

---
+++ Sort.h
@@ -0,0 +1,151 @@
+#ifndef SORT_H
+#define SORT_H
+
+/**
+ * Several sorting routines.
+ * Arrays are rearranged with smallest item first.
+ */
+
+#include <vector>
+#include <functional>
+using namespace std;
+
+/**
+ * Simple insertion sort.
+ */
+template <typename Comparable>
+void insertionSort( vector<Comparable> & a )
+{
+    for( int p = 1; p < a.size( ); ++p )
+    {
+        Comparable tmp = std::move( a[ p ] );
+
+        int j;
+        for( j = p; j > 0 && tmp < a[ j - 1 ]; --j )
+            a[ j ] = std::move( a[ j - 1 ] );
+        a[ j ] = std::move( tmp );
+    }
+}
+
+
+/**
+ * Internal insertion sort routine for subarrays
+ * that is used by quicksort.
+ * a is an array of Comparable items.
+ * left is the left-most index of the subarray.
+ * right is the right-most index of the subarray.
+ */
+template <typename Comparable>
+void insertionSort( vector<Comparable> & a, int left, int right )
+{
+    for( int p = left + 1; p <= right; ++p )
+    {
+        Comparable tmp = std::move( a[ p ] );
+        int j;
+
+        for( j = p; j > left && tmp < a[ j - 1 ]; --j )
+            a[ j ] = std::move( a[ j - 1 ] );
+        a[ j ] = std::move( tmp );
+    }
+}
+
+
+
+/**
+ * Shellsort, using Shell's (poor) increments.
+ */
+template <typename Comparable>
+void shellsort( vector<Comparable> & a )
+{
+    for( int gap = a.size( ) / 2; gap > 0; gap /= 2 )
+        for( int i = gap; i < a.size( ); ++i )
+        {
+            Comparable tmp = std::move( a[ i ] );
+            int j = i;
+
+            for( ; j >= gap && tmp < a[ j - gap ]; j -= gap )
+                a[ j ] = std::move( a[ j - gap ] );
+            a[ j ] = std::move( tmp );
+        }
+}
+
+/**
+ * Standard heapsort.
+ */
+template <typename Comparable>
+void heapsort( vector<Comparable> & a )
+{
+    for( int i = a.size( ) / 2 - 1; i >= 0; --i )  /* buildHeap */
+        percDown( a, i, a.size( ) );
+    for( int j = a.size( ) - 1; j > 0; --j )
+    {
+        std::swap( a[ 0 ], a[ j ] );               /* deleteMax */
+        percDown( a, 0, j );
+    }
+}
+
+/**
+ * Internal method for heapsort.
+ * i is the index of an item in the heap.
+ * Returns the index of the left child.
+ */
+inline int leftChild( int i )
+{
+    return 2 * i + 1;
+}
+
+/**
+ * Internal method for heapsort that is used in
+ * deleteMax and buildHeap.
+ * i is the position from which to percolate down.
+ * n is the logical size of the binary heap.
+ */
+template <typename Comparable>
+void percDown( vector<Comparable> & a, int i, int n )
+{
+    int child;
+    Comparable tmp;
+
+    for( tmp = std::move( a[ i ] ); leftChild( i ) < n; i = child )
+    {
+        child = leftChild( i );
+        if( child != n - 1 && a[ child ] < a[ child + 1 ] )
+            ++child;
+        if( tmp < a[ child ] )
+            a[ i ] = std::move( a[ child ] );
+        else
+            break;
+    }
+    a[ i ] = std::move( tmp );
+}
+
+/**
+ * Internal method that makes recursive calls.
+ * a is an array of Comparable items.
+ * tmpArray is an array to place the merged result.
+ * left is the left-most index of the subarray.
+ * right is the right-most index of the subarray.
+ */
+template <typename Comparable>
+void mergeSort( vector<Comparable> & a,
+                vector<Comparable> & tmpArray, int left, int right )
+{
+    if( left < right )
+    {
+        int center = ( left + right ) / 2;
+        mergeSort( a, tmpArray, left, center );
+        mergeSort( a, tmpArray, center + 1, right );
+        merge( a, tmpArray, left, center + 1, right );
+    }
+}
+
+/**
+ * Mergesort algorithm (driver).
+ */
+template <typename Comparable>
+void mergeSort( vector<Comparable> & a )
+{
+    vector<Comparable> tmpArray( a.size( ) );
+
+    mergeSort( a, tmpArray, 0, a.size( ) - 1 );
+}
