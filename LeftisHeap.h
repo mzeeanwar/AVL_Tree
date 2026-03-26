33b37d58157b89d69530c4c72d71be42c3df4714
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-03-26 22:07:28 +0500

    Update LeftisHeap.h

--- LeftisHeap.h
+++ LeftisHeap.h
@@ -226,3 +226,34 @@ class LeftistHeap
 };
 
 #endif
+void insert( const Comparable & x )
+    {
+        static BinaryNode *newNode = nullptr;
+
+        if( newNode == nullptr )
+            newNode = new BinaryNode;
+        newNode->element = x;
+
+        if( root == nullNode )
+        {
+            newNode->left = newNode->right = nullNode;
+            root = newNode;
+        }
+        else
+        {
+            splay( x, root );
+            if( x < root->element )
+            {
+                newNode->left = root->left;
+                newNode->right = root;
+                root->left = nullNode;
+                root = newNode;
+            }
+            else
+            if( root->element < x )
+            {
+                newNode->right = root->right;
+                newNode->left = root;
+                root->right = nullNode;
+                root = newNode;
+            }
