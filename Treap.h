f5938f807b728ef5f449b9b1cdba01fd63ae83fd
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-03-28 23:04:08 +0500

    Treap.h

--- Treap.h
+++ Treap.h
@@ -215,3 +215,78 @@ TreapNode *root;
      * t is the node that roots the tree.
      * Set the new root of the subtree.
      */
+void remove( const Comparable & x, TreapNode * & t )
+    {
+        if( t != nullNode )
+        {
+            if( x < t->element )
+                remove( x, t->left );
+            else if( t->element < x )
+                remove( x, t->right );
+            else
+            {
+                    // Match found
+                if( t->left->priority < t->right->priority )
+                    rotateWithLeftChild( t );
+                else
+                    rotateWithRightChild( t );
+
+                if( t != nullNode )      // Continue on down
+                    remove( x, t );
+                else
+                {
+                    delete t->left;
+                    t->left = nullNode;  // At a leaf
+                }
+            }
+        }
+    }
+
+    void makeEmpty( TreapNode * & t )
+    {
+        if( t != nullNode )
+        {
+            makeEmpty( t->left );
+            makeEmpty( t->right );
+            delete t;
+        }
+        t = nullNode;
+    }
+
+    void printTree( TreapNode *t ) const
+    {
+        if( t != nullNode )
+        {
+            printTree( t->left );
+            cout << t->element << endl;
+            printTree( t->right );
+        }
+    }
+
+        // Rotations
+    void rotateWithLeftChild( TreapNode * & k2 )
+    {
+        TreapNode *k1 = k2->left;
+        k2->left = k1->right;
+        k1->right = k2;
+        k2 = k1;
+    }
+
+    void rotateWithRightChild( TreapNode * & k1 )
+    {
+        TreapNode *k2 = k1->right;
+        k1->right = k2->left;
+        k2->left = k1;
+        k1 = k2;
+    }
+
+    TreapNode * clone( TreapNode * t ) const
+    {
+        if( t == t->left )  // Cannot test against nullNode!!!
+            return nullNode;
+        else
+            return new TreapNode{ t->element, clone( t->left ), clone( t->right ), t->priority };
+    }
+};
+
+#endif
