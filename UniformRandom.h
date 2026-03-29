8717923784eb252aad35bb0f62bb58f870c3c7bd
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-03-29 23:57:13 +0500

    UniformRandom.h

--- UniformRandom.h
+++ UniformRandom.h
@@ -110,3 +110,72 @@ class Random48
         return state >> ( 48 - bits );
     }
 };
+// UniformRandom class
+//
+// CONSTRUCTION: with (a) no initializer or (b) a int
+//     that specifies the initial state of the generator.
+//     This random number generator uses the 32-bit Mersenne Twister
+//
+// ******************PUBLIC OPERATIONS*********************
+//     Return a random number according to some distribution:
+// int nextInt( )                          --> Uniform
+// int nextDouble( )                       --> Uniform, [0 to 1)
+// int nextInt( int high )                 --> Uniform [0..high)
+// int nextInt( int low, int high )        --> Uniform [low..high)
+
+
+static int currentTimeSeconds( )
+{
+    auto now = chrono::high_resolution_clock::now( ).time_since_epoch( );       
+    return chrono::duration_cast<chrono::seconds>( now ).count( );
+}
+    
+/**
+ * Uniform Random Number generator class, using C++ Mersenne Twister.
+ */
+class UniformRandom
+{
+  public:
+    UniformRandom( int seed = currentTimeSeconds( ) ) : generator{ seed }
+    {
+    }
+      
+    /**
+     * Return a pseudorandom int.
+     */
+    int nextInt( )
+    {
+        static uniform_int_distribution<unsigned int> distribution;
+        return distribution( generator );
+    }
+
+    /**
+     * Return a pseudorandom int in range [0..high).
+     */
+    int nextInt( int high )
+    {
+        return nextInt( 0, high - 1 ); 
+    }
+    
+    /**
+     * Return a pseudorandom double in the range [0..1).
+     */
+    double nextDouble( )
+    {
+        static uniform_real_distribution<double> distribution( 0, 1 );
+        return distribution( generator );
+    }
+
+    /**
+     * Return an int in the closed range [low,high].
+     */
+    int nextInt( int low, int high )
+    {
+        uniform_int_distribution<int> distribution( low, high );
+        return distribution( generator );
+    }
+    
+  private:
+    mt19937 generator;
+};
+#endif
