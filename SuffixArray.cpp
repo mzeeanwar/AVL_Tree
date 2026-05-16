ac2a062604a01aaa33c2a5d9335dc0ea6dd8582f
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-16 22:55:14 +0500

    Add function declarations for suffix array operations

--- SuffixArray.cpp
+++ SuffixArray.cpp
@@ -29,3 +29,14 @@ void merge( const vector<int> & s, const vector<int> & s12,
             vector<int> & SA, const vector<int> & SA0, const vector<int> & SA12,
                             int n, int n0, int n12, int t );
 int getIndexIntoS( const vector<int> & SA12, int t, int n0 );
+bool leq( int a1, int a2, int b1, int b2 );
+bool leq( int a1, int a2, int a3, int b1, int b2, int b3 );
+bool suffix12IsSmaller( const vector<int> & s, const vector<int> & s12,
+                        const vector<int> & SA12, int n0, int i, int j, int t );
+void printV( const vector<int> & a, const string & comment);
+bool isPermutation( const vector<int> & SA, int n );
+bool sleq( const vector<int> & s1, int start1, const vector<int> & s2, int start2 );
+bool isSorted( const vector<int> & SA, const vector<int> & s, int n );
+void assert0( bool cond );
+int computeLCP( const string & s1, const string & s2 );
+void createSuffixArraySlow( const string & str, vector<int> & SA, vector<int> & LCP );
