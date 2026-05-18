caceb75d93c4ee18f6c09db813bc081226ca1524
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-19 01:54:17 +0500

    Add test for LeftistHeap insertion and merging

--- TestLeftisHeap.cpp
+++ TestLeftisHeap.cpp
@@ -9,3 +9,11 @@ int main( )
     LeftistHeap<int> h1;
     LeftistHeap<int> h2;
     int i = 37;
+    cout << "Begin test..." << endl;
+    for( i = 37; i != 0; i = ( i + 37 ) % numItems )
+        if( i % 2 == 0 )
+            h1.insert( i );
+        else
+            h.insert( i );
+    h.merge( h1 );
+    h2 = h;
