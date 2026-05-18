39c9aad8eec832fc25bd984ac8d0025797934e5b
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-19 01:53:51 +0500

    Add initial test for LeftistHeap implementation

---
+++ TestLeftisHeap.cpp
@@ -0,0 +1,11 @@
+#include "LeftistHeap.h"
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+    int numItems = 10000;
+    LeftistHeap<int> h;
+    LeftistHeap<int> h1;
+    LeftistHeap<int> h2;
+    int i = 37;
