3f3e58b83470962e23637a4a7ece2dd387bda039
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-21 02:29:13 +0500

    Implement Stack class template in TestList.cpp

---
+++ TestList.cpp
@@ -0,0 +1,27 @@
+#include "dsexceptions.h"
+#include "List.h"
+#include "Vector.h"
+
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+static const int NUMS_PER_LINE = 14;
+
+
+template <typename Object>
+class Stack 
+{
+  public:
+    bool isEmpty( ) const
+      { return theList.empty( ); }
+    const Object & top( ) const
+      { return theList.front( ); }
+    void push( const Object & x )
+      { theList.push_front( x ); }
+    void pop( Object & x )
+      { x = theList.front( ); theList.pop_front( ); }
+  private:
+    List<Object> theList;
+};
