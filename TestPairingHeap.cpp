09291ade3801ba3f1e8a6c8c655ef009e91f49ae
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-21 02:37:04 +0500

    Implement test program for PairingHeap
    Added a test program for the PairingHeap implementation.

---
+++ TestPairingHeap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "PairingHeap.h"
+using namespace std;
+
+    // Test program
+int main( )
+{
+    PairingHeap<int> h;
+
+    int numItems = 4000;
+    int i = 37;
+    int j;
+
+    cout << "Checking; no bad output is good" << endl;
+    for( i = 37; i != 0; i = ( i + 37 ) % numItems )
+        h.insert( i );
