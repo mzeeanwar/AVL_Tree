1ea09206529d78a23bea7d7312e2f93b283c9119
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-22 01:19:35 +0500

    Enhance tests for Red-Black Tree functionality
    Added checks for findMin, findMax, and contains methods.

--- TestRedBlackTree.cpp
+++ TestRedBlackTree.cpp
@@ -15,3 +15,13 @@ int main( )
 
     for( i = GAP; i != 0; i = ( i + GAP ) % NUMS )
         t.insert( i );
+    if( NUMS < 40 )
+        t.printTree( );
+    if( t.findMin( ) != 1 || t.findMax( ) != NUMS - 1 )
+        cout << "FindMin or FindMax error!" << endl;
+
+    for( i = 1; i < NUMS; ++i )
+        if( !t.contains( i ) )
+            cout << "Find error1!" << endl;
+    if( t.contains( 0 ) )
+        cout << "Oops!" << endl;
