7cc9a7d5fbc2df716b6f64628b072bd2ce1d86d1
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-23 00:39:21 +0500

    Implement setUnion method in DisjSets class
    Added documentation for setUnion method and implemented union logic.

--- TestSlowDisjSets.cpp
+++ TestSlowDisjSets.cpp
@@ -37,3 +37,15 @@ DisjSets::DisjSets( int numElements ) : s( numElements )
     for( auto & link : s )
         link = -1;
 }
+
+/**
+ * Union two disjoint sets.
+ * For simplicity, we assume root1 and root2 are distinct
+ * and represent set names.
+ * root1 is the root of set 1.
+ * root2 is the root of set 2.
+ */
+void DisjSets::setUnion( int root1, int root2 )
+{
+    s[ root2 ] = root1;
+}
