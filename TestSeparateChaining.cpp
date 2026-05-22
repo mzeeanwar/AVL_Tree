07b1227b5d30c8373fc8559bc62deab5ce203a59
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-23 00:32:20 +0500

    Add tests for separate chaining hash table

--- TestSeparateChaining.cpp
+++ TestSeparateChaining.cpp
@@ -13,3 +13,15 @@ int main( )
     int i;
 
     cout << "Checking... (no more output means success)" << endl;
+
+    for( i = GAP; i != 0; i = ( i + GAP ) % NUMS )
+        h1.insert( i );
+    
+    h2 = h1;
+    
+    for( i = 1; i < NUMS; i += 2 )
+        h2.remove( i );
+
+    for( i = 2; i < NUMS; i += 2 )
+        if( !h2.contains( i ) )
+            cout << "Contains fails " << i << endl;
