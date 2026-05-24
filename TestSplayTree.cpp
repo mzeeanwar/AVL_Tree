711a6d47e6402381f634ccc7b7ca1a60eda3c76f
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-24 21:44:28 +0500

    Add tests for SplayTree copy and contains method

--- TestSplayTree.cpp
+++ TestSplayTree.cpp
@@ -31,3 +31,20 @@
             if( t.contains( i ) )
                 cout << "Find error2!" << endl;
         }
+
+        SplayTree<int> t2;
+        t2 = t;
+
+        for( i = 2; i < NUMS; i+=2 )
+            if( !t2.contains( i ) )
+                cout << "Find error1!" << endl;
+
+        for( i = 1; i < NUMS; i+=2 )
+        {
+            if( t2.contains( i ) )
+                cout << "Find error2!" << endl;
+        }
+
+        cout << "Test completed." << endl;
+        return 0;
+}
