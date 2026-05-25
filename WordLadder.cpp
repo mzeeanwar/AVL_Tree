1f16778bbab9fb58f48a9ca13dd873734b1def12
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-25 11:58:57 +0500

    Update WordLadder.cpp

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -74,3 +74,34 @@ computeAdjacentWordsSlow( const vector<string> & words )
 
     return adjWords;
 }
+
+// Computes a map in which the keys are words and values are vectors of words
+// that differ in only one character from the corresponding key.
+// Uses a quadratic algorithm, but speeds things up a little by
+// maintaining an additional map that groups words by their length.
+map<string,vector<string>>
+computeAdjacentWordsMedium( const vector<string> & words )
+{
+    map<string,vector<string>> adjWords;
+    map<int,vector<string>> wordsByLength;
+
+      // Group the words by their length
+    for( auto & thisWord : words )
+        wordsByLength[ thisWord.length( ) ].push_back( thisWord );
+
+      // Work on each group separately
+    for( auto & entry : wordsByLength )
+    {
+        const vector<string> & groupsWords = entry.second;
+
+        for( int i = 0; i < groupsWords.size( ); ++i )
+            for( int j = i + 1; j < groupsWords.size( ); ++j )
+                if( oneCharOff( groupsWords[ i ], groupsWords[ j ] ) )
+                {
+                    adjWords[ groupsWords[ i ] ].push_back( groupsWords[ j ] );
+                    adjWords[ groupsWords[ j ] ].push_back( groupsWords[ i ] );
+                }
+    }
+
+    return adjWords;
+}
