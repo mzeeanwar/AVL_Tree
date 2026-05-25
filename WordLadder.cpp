c9d7df57152e6e962f082c323320b229fe795278
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-25 11:58:19 +0500

    Update WordLadder.cpp

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -56,3 +56,21 @@ bool oneCharOff( const string & word1, const string & word2 )
 
     return diffs == 1;
 }
+// Computes a map in which the keys are words and values are vectors of words
+// that differ in only one character from the corresponding key.
+// Uses a quadratic algorithm.
+map<string,vector<string>>
+computeAdjacentWordsSlow( const vector<string> & words )
+{
+    map<string,vector<string>> adjWords;
+
+    for( int i = 0; i < words.size( ); ++i )
+        for( int j = i + 1; j < words.size( ); ++j )
+            if( oneCharOff( words[ i ], words[ j ] ) )
+            {
+                adjWords[ words[ i ] ].push_back( words[ j ] );
+                adjWords[ words[ j ] ].push_back( words[ i ] );
+            }
+
+    return adjWords;
+}
