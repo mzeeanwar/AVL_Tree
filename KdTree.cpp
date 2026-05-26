fe77202ddbf9091501f67b3a28e910b2fa3f0d64
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-27 03:45:43 +0500

    Update KdTree.cpp

--- KdTree.cpp
+++ KdTree.cpp
@@ -16,3 +16,25 @@ class KdTree
     {
         insert( x, root, 0 );
     }
+
+    /**
+     * Print items satisfying
+     * low[ 0 ] <= x[ 0 ] <= high[ 0 ] and
+     * low[ 1 ] <= x[ 1 ] <= high[ 1 ]
+     */
+    void printRange( const vector<Comparable> & low, 
+                        const vector<Comparable> & high ) const
+    {
+        printRange( low, high, root, 0 );
+    }
+
+  private:
+    struct KdNode
+    {
+        vector<Comparable> data;
+        KdNode            *left;
+        KdNode            *right;
+
+        KdNode( const vector<Comparable> & item )
+          : data( item ), left( nullptr ), right( nullptr ) { }
+    };
