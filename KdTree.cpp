d0b39d7c04463e43caf926bf7bd80c15b6260eb1
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-27 03:47:39 +0500

    Update KdTree.cpp

--- KdTree.cpp
+++ KdTree.cpp
@@ -68,3 +68,27 @@ class KdTree
         }
     }
 };
+// Test program
+int main( )
+{
+    KdTree<int> t;
+    
+    cout << "Starting program" << endl;
+    for( int i = 300; i < 370; ++i )
+    {
+        vector<int> it( 2 );
+        it[ 0 ] =  i;
+        it[ 1 ] =  2500 - i;
+        t.insert( it );
+    }
+
+    vector<int> low( 2 ), high( 2 );
+    low[ 0 ] = 70;
+    low[ 1 ] = 2186;
+    high[ 0 ] = 1200;
+    high[ 1 ] = 2200;
+
+    t.printRange( low, high );
+
+    return 0;
+}
