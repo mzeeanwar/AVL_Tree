4cc11946df6c092bb632a64625f0d489836af999
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-28 06:06:11 +0500

    Update MaxSumTest.cpp

--- MaxSumTest.cpp
+++ MaxSumTest.cpp
@@ -52,3 +52,39 @@ int max3( int a, int b, int c )
 {
     return a > b ? a > c ? a : c : b > c ? b : c;
 }
+/**
+ * Recursive maximum contiguous subsequence sum algorithm.
+ * Finds maximum sum in subarray spanning a[left..right].
+ * Does not attempt to maintain actual best sequence.
+ */
+int maxSumRec( const vector<int> & a, int left, int right )
+{
+    if( left == right )  // Base case
+        if( a[ left ] > 0 )
+            return a[ left ];
+        else
+            return 0;
+
+    int center = ( left + right ) / 2;
+    int maxLeftSum  = maxSumRec( a, left, center );
+    int maxRightSum = maxSumRec( a, center + 1, right );
+
+    int maxLeftBorderSum = 0, leftBorderSum = 0;
+    for( int i = center; i >= left; --i )
+    {
+        leftBorderSum += a[ i ];
+        if( leftBorderSum > maxLeftBorderSum )
+            maxLeftBorderSum = leftBorderSum;
+    }
+
+    int maxRightBorderSum = 0, rightBorderSum = 0;
+    for( int j = center + 1; j <= right; ++j )
+    {
+        rightBorderSum += a[ j ];
+        if( rightBorderSum > maxRightBorderSum )
+            maxRightBorderSum = rightBorderSum;
+    }
+
+    return max3( maxLeftSum, maxRightSum,
+                    maxLeftBorderSum + maxRightBorderSum );
+}
