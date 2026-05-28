e3fdb1c486442b5bb265229488e6668544c0dbcc
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-28 06:06:55 +0500

    Update MaxSumTest.cpp

--- MaxSumTest.cpp
+++ MaxSumTest.cpp
@@ -96,3 +96,22 @@ int maxSubSum3( const vector<int> & a )
 {
     return maxSumRec( a, 0, a.size( ) - 1 );
 }
+/**
+ * Linear-time maximum contiguous subsequence sum algorithm.
+ */
+int maxSubSum4( const vector<int> & a )
+{
+    int maxSum = 0, thisSum = 0;
+
+    for( int j = 0; j < a.size( ); ++j )
+    {
+        thisSum += a[ j ];
+
+        if( thisSum > maxSum )
+            maxSum = thisSum;
+        else if( thisSum < 0 )
+            thisSum = 0;
+    }
+
+    return maxSum;
+}
